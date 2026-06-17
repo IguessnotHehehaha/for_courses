3742b605013e094d28f4584ceabf2456aeadd102
IguessnotHehehaha <tnurikhanov@gmail.com>
2026-06-17 13:01:58 +0500

    undid the crutch for the 404

--- task4/client/src/App.jsx
+++ task4/client/src/App.jsx
@@ -1,23 +1,11 @@
-import { useEffect } from 'react'
-import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Users from './pages/Users'
 
-function RedirectHandler() {
-    const navigate = useNavigate()
-    useEffect(() => {
-        const params = new URLSearchParams(window.location.search)
-        const redirect = params.get('redirect')
-        if (redirect) navigate(redirect, { replace: true })
-    }, [])
-    return null
-}
-
 export default function App() {
     return (
         <BrowserRouter>
-            <RedirectHandler />
             <Routes>
                 <Route path="/" element={<Navigate to="/login" />} />
                 <Route path="/login" element={<Login />} />
